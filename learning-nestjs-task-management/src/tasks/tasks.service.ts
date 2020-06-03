7651f8385330dbbc038e72185b8501256235f6bc
Túlio Moura <tulio.moura@vitta.me>
2020-06-03 09:51:31 -0300

    feature: adding validation on create and update tasks

--- learning-nestjs-task-management/src/tasks/tasks.service.ts
+++ learning-nestjs-task-management/src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDTO } from './dto/create-task.dto';
@@ -13,7 +13,10 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id == id);
+    const found = this.tasks.find(task => task.id == id);
+    if (!found)
+      throw new NotFoundException(`Task with ID " ${id}" not found`);
+    return found;
   }
 
   getTasks(filter: GetTaskFilterDTO): Task[] {
@@ -50,6 +53,7 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter(task => task.id !== found.id);
   }
 }
